467f7128b0f7a68b23feded933a152ed74461b1a
Sophia <liu.siyu5@northeastern.edu>
2024-10-04 06:43:49 -0400

    added once again

---
+++ LAB0/src/subscriber.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo("I heard %s", data.data)
+
+def subscriber():
+    rospy.init_node('subscriber', anonymous=True)
+    
+    rospy.Subscriber('chatter', String, callback)
+    
+    rospy.spin()
+
+if __name__ == '__main__':
+    subscriber()
