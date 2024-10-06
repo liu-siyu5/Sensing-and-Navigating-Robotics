328674383792ebb2ffd827cf9a9e6d8acbea0468
liu-siyu5 <liu.siyu5@northeastern.edu>
2024-10-05 22:33:51 -0400

    Remove <folder_name> folder

---
+++ LAB00/src/beginner_tutorials/scripts/talker.py
@@ -0,0 +1,20 @@
+
+
+import rospy
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        snc = "hey iam sachi!!  %s" % rospy.get_time()
+        rospy.loginfo(snc)
+        pub.publish(snc)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
